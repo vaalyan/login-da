aca773f70b807bc9f1c5308d14f75d7acb7d7502
vaalyan <vaalyan32@gmail.com>
2024-03-09 02:06:19 -0500

    muchos cambios

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "daniel2024", "login_register_db", 33066);
+
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+
+    */
+?>
